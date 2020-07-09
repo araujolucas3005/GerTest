1e1f492467589c6e9a4606b0bbae49c5312e8764
Natalio Silva Junior <natalio.junior18@gmail.com>
2020-07-09 17:46:25 -0300

    Implementação do DisciplinaBO

--- src/model/BO/DisciplinaBO.java
+++ src/model/BO/DisciplinaBO.java
@@ -1,6 +1,24 @@
 package model.BO;
+import java.util.ArrayList;
+import java.util.List;
+
 import model.VO.DisciplinaVO;
 
 public class DisciplinaBO {
-
+	
+	public void cadastrar(DisciplinaVO disciplina) {
+		 List<DisciplinaVO> exemplo = new ArrayList<DisciplinaVO>();
+		 exemplo.add(disciplina);
+	}
+		
+	public void editar(DisciplinaVO disciplina) {
+		disciplina.setNome("banco de dados");
+		disciplina.setCodigo("EXA101");
+	}
+	
+	public void remover(DisciplinaVO disciplina) {
+		List<DisciplinaVO> exemplo = new ArrayList<DisciplinaVO>();
+		exemplo.remove(disciplina);	
+	
+	}
 }
