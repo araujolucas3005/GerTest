6dc37013350f201f4c733d3ff297fe966aa13ee1
Natalio Silva Junior <natalio.junior18@gmail.com>
2020-07-15 17:53:47 -0300

    Remorção da QuestaoInterDAO para Adição do BaseInterDAO

---
+++ src/model/DAO/BaseInterDAO.java
@@ -0,0 +1,12 @@
+package model.DAO;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public interface BaseInterDAO<VO> {
+	public abstract void inserir(VO vo)throws SQLException;
+	public abstract void remover(VO vo)throws SQLException;
+	public abstract void atualizar(VO vo) throws SQLException;
+	public abstract ResultSet listar() throws SQLException;
+	public abstract ResultSet listarPorId(VO vo) throws SQLException;
+}
