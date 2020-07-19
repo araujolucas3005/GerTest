d1850dd5f16f77d92948ca8e21dafade5131958b
Lucas <araujolucas3005@gmail.com>
2020-07-18 21:45:08 -0300

    corrigindo alguns erros

--- src/model/BO/DisciplinaBO.java
+++ src/model/BO/DisciplinaBO.java
@@ -3,22 +3,19 @@ package model.BO;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Random;
+import exception.InsertException;
 import java.util.List;
-
 import model.DAO.DisciplinaDAO;
 import model.VO.BiologicaVO;
 import model.VO.DisciplinaVO;
 import model.VO.ExataVO;
 import model.VO.HumanaVO;
-import model.VO.ProvaVO;
-import model.VO.QuestaoVO;
 
 public class DisciplinaBO extends BaseBO<DisciplinaVO> {
 
 	private static DisciplinaDAO dDao = new DisciplinaDAO();
 
-	public void cadastrar(DisciplinaVO disciplina) {
+	public void cadastrar(DisciplinaVO disciplina) throws InsertException {
 		ResultSet rs = dDao.listarPorCodigo(disciplina);
 		ResultSet rs2 = dDao.listarPorNome(disciplina);
 		try {
@@ -38,30 +35,35 @@ public class DisciplinaBO extends BaseBO<DisciplinaVO> {
 	public DisciplinaVO buscarPorId(DisciplinaVO disciplina) {
 		ResultSet rs = dDao.listarPorId(disciplina);
 		DisciplinaVO disc = null;
-		try {
-			if (rs.next()) {
-				if (rs.getString("Codigo").substring(0, 3).equals("EXA")) {
-					disc = new ExataVO();
-					disc.setId(rs.getLong("id"));
-					disc.setCodigo(rs.getString("codigo"));
-					disc.setNome(rs.getString("nome"));
-				} else if (rs.getString("Codigo").substring(0, 3).equals("BIO")) {
-					disc = new BiologicaVO();
-					disc.setId(rs.getLong("id"));
-					disc.setCodigo(rs.getString("codigo"));
-					disc.setNome(rs.getString("nome"));
+
+			try {
+				if (rs.next()) {
+					if (rs.getString("Codigo").substring(0, 3).equals("EXA")) {
+						disc = new ExataVO();
+						disc.setId(rs.getLong("id"));
+						disc.setCodigo(rs.getString("codigo"));
+						disc.setNome(rs.getString("nome"));
+					} else if (rs.getString("Codigo").substring(0, 3).equals("BIO")) {
+						disc = new BiologicaVO();
+						disc.setId(rs.getLong("id"));
+						disc.setCodigo(rs.getString("codigo"));
+						disc.setNome(rs.getString("nome"));
+					} else {
+						disc = new HumanaVO();
+						disc.setId(rs.getLong("id"));
+						disc.setCodigo(rs.getString("codigo"));
+						disc.setNome(rs.getString("nome"));
+					}
 				} else {
-					disc = new HumanaVO();
-					disc.setId(rs.getLong("id"));
-					disc.setCodigo(rs.getString("codigo"));
-					disc.setNome(rs.getString("nome"));
+					throw new InsertException("Não existe disciplina com esse ID!");
 				}
-			} else {
-				throw new InsertException("Não existe disciplina com esse ID!");
+			} catch (InsertException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		} catch (SQLException e) {
-			throw new InsertException(e.getMessage());
-		}
 		return disc;
 	}
 
@@ -70,57 +72,66 @@ public class DisciplinaBO extends BaseBO<DisciplinaVO> {
 		List<DisciplinaVO> disciplinas = new ArrayList<>();
 		DisciplinaVO disciplina = null;
 
-		try {
-			while (rs.next()) {
-				if (rs.getString("Codigo").substring(0, 3).equals("EXA")) {
-					disciplina = new ExataVO();
-					disciplina.setId(rs.getLong("id"));
-					disciplina.setCodigo(rs.getString("codigo"));
-					disciplina.setNome(rs.getString("nome"));
-				} else if (rs.getString("Codigo").substring(0, 3).equals("BIO")) {
-					disciplina = new BiologicaVO();
-					disciplina.setId(rs.getLong("id"));
-					disciplina.setCodigo(rs.getString("codigo"));
-					disciplina.setNome(rs.getString("nome"));
-				} else {
-					disciplina = new HumanaVO();
-					disciplina.setId(rs.getLong("id"));
-					disciplina.setCodigo(rs.getString("codigo"));
-					disciplina.setNome(rs.getString("nome"));
+
+			try {
+				while (rs.next()) {
+					if (rs.getString("Codigo").substring(0, 3).equals("EXA")) {
+						disciplina = new ExataVO();
+						disciplina.setId(rs.getLong("id"));
+						disciplina.setCodigo(rs.getString("codigo"));
+						disciplina.setNome(rs.getString("nome"));
+					} else if (rs.getString("Codigo").substring(0, 3).equals("BIO")) {
+						disciplina = new BiologicaVO();
+						disciplina.setId(rs.getLong("id"));
+						disciplina.setCodigo(rs.getString("codigo"));
+						disciplina.setNome(rs.getString("nome"));
+					} else {
+						disciplina = new HumanaVO();
+						disciplina.setId(rs.getLong("id"));
+						disciplina.setCodigo(rs.getString("codigo"));
+						disciplina.setNome(rs.getString("nome"));
+					}
+					disciplinas.add(disciplina);
 				}
-				disciplinas.add(disciplina);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		} catch (SQLException e) {
-			throw new InsertException(e.getMessage());
-		}
+
 		return disciplinas;
 	}
 
 	public void alterar(DisciplinaVO disciplina) {
 		ResultSet rs = dDao.listarPorCodigo(disciplina);
-		try {
-			if (rs.next()) {
-				throw new InsertException("Ja existe disciplina com esse codigo!");
-			} else {
-				dDao.atualizar(disciplina);
+			try {
+				if (rs.next()) {
+					throw new InsertException("Ja existe disciplina com esse codigo!");
+				} else {
+					dDao.atualizar(disciplina);
+				}
+			} catch (InsertException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		} catch (SQLException e) {
-			throw new InsertException(e.getMessage());
-
-		}
 	}
 
 	public void remover(DisciplinaVO disciplina) {
 		ResultSet rs = dDao.listarPorCodigo(disciplina);
-		try {
-			if (!rs.next()) {
-				throw new InsertException("Essa disciplina não existe!");
-			} else {
-				dDao.remover(disciplina);
+			try {
+				if (rs.next()) {
+					dDao.remover(disciplina);
+				} else {
+					throw new InsertException("essa disciplina nao existe!");
+				}
+			} catch (InsertException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		} catch (SQLException e) {
-			throw new SQLException(e.getMessage());
-
-		}
 	}
 }
